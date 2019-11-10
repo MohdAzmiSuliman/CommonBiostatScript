ae6c1364d25c728787ffbb0f16a75ce9b34ae7e2
puterabemi <beck_mcgerg@yahoo.com>
2019-11-10 08:11:07 +0000

    Added 2 new script. Update DataWrangling script

---
+++ CommonLibrary.R
@@ -0,0 +1,45 @@
+# Common Library ----
+
+## to be recategorize
+
+library(psych)
+library(ResourceSelection)
+
+
+## Import Data ----
+
+library(haven)
+###
+
+## Tools packages ----
+
+library(dplyr)
+### for %>% function
+
+
+## Result ----
+
+library(tidyr)
+
+library(knitr)
+### for kable function
+
+library(broom)
+### nice table
+
+## Plot ----
+
+library(ggplot)
+### for various plot
+
+library(corrplot)
+### correlation plot
+
+## Regression ----
+
+library(generalhoslem)
+### Hosmer Lemeshow Packages
+
+library(mfp)
+
+library(LogisticDx)
--- DataWrangling.R
+++ DataWrangling.R
@@ -1,13 +1,36 @@
 # Data Wrangling ----
 
+## Import Data ----
+
+DataSet <- read_dta("files.dta")
+### read .dta file (stata)
+### require haven package
+
+DataSet$VarName <- as.factor(DataSet$VarName)
+### when using haven::read_dta, unable to convert directly categorical data
+### !! warning: it will overwrite the variables. use other variables name if want to preserve the original variable
+
+DataSet$VarRename <- as.factor(DataSet$VarName)
+### same with above, but create new variables
+
+DataSet2 <- DataSet %>% mutate (VarRename = as_factor(VarName))
+### same with above
+### require dplyr package
+
+
 ## Data Summary ----
 
 str(object)
-# display internal structure of data
+### display internal structure of data
 
 view(object)
-# open object to viewer (not adviseable for rmd)
+### open object to viewer (not adviseable for rmd)
 
 head(object)
-# display first several data 
+### display first several data 
 
+kable(object)
+### display 
+### require knitr packages
+
+summary(object)
