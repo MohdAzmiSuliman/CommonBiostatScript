b13be224ac261f107d4c73417a9621b5709a8b92
puterabemi <beck_mcgerg@yahoo.com>
2019-11-17 01:43:05 +0000

    add Cox PH

--- SurvivalAnalysis.R
+++ SurvivalAnalysis.R
@@ -7,3 +7,22 @@ library (survival)
 
 library()
 ### for plot survival 
+
+## Univariable
+
+survfit(Surv(time = VarTime, event == "ObsOutcome") ~ 1,
+                 data = DataSet,
+                 type = "kaplan-meier")
+### KM Estimate for overall
+### VarTime - duration of event (numerical), if only date variable is available, calculate duration using lubridate function
+
+survfit(Surv(time = VarTime, event == "ObsOutcome") ~ VarCat1,
+        data = DataSet,
+        type = "kaplan-meier")
+### KM Estimate for categorical variable (VarCat1)
+
+coxph(Surv(time = VarTime, event == "ObsOutcome") ~ VarNum1,
+        data = DataSet)
+### Cox Proportional Hazard for numerical variable (VarNum1)
+        
+
